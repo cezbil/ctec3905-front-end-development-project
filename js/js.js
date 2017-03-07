3214c5ddda174b7ca3858e005e5824a0e26440b7
cezbil <czaro93pl@gmail.com>
2017-03-07 20:42:32 +0000

    different background some additional fonts

--- js/js.js
+++ js/js.js
@@ -6,6 +6,7 @@ function hideElement(ele) {
 function changeContent(option) {
     var contentChildrens = document.getElementById("content").getElementsByTagName("div");
 
+
     for(var i = 0; i < contentChildrens.length; i++) {
         contentChildrens[i].style.animationDuration = "4s";
         contentChildrens[i].style.animationName = "hide-sidebar";
@@ -15,7 +16,7 @@ function changeContent(option) {
         document.getElementById("about-mma-content").style.display = "block";
     }
     else if(option == "boxing") {
-        document.getElementById("boxing-content").style.animationDuration = "4s";
+       document.getElementById("boxing-content").style.animationDuration = "4s";
         document.getElementById("boxing-content").style.animationName = "show-sidebar";
     }
     else if(option == "jiujitsu") {
